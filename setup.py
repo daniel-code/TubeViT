b13ce67ad09f82a0c882de7412ed5f7a0596741f
Su YR <suyr.daniel@gmail.com>
2023-01-15 14:13:04 +0800

    chore: setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='TubeVit',
+    packages=find_packages(),
+    version='0.1.0',
+    description='implement Rethinking Video ViTs: Sparse Video Tubes for Joint Image and Video Learning.',
+    author='Su YR',
+    license="MIT",
+)
