2a5b4b620ce64bd35f79e22242ff84535bee520c
Su YR <suyr.daniel@gmail.com>
2023-02-12 23:41:26 +0800

    chore: update description

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
+# yapf: disable
 setup(
     name='TubeVit',
     packages=find_packages(),
     version='0.1.0',
-    description='implement Rethinking Video ViTs: Sparse Video Tubes for Joint Image and Video Learning.',
+    description='An unofficial implementation of TubeViT in "Rethinking Video ViTs: Sparse Video Tubes for Joint Image and Video Learning".',
     author='Su YR',
     license="MIT",
 )
