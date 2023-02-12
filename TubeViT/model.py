988005cb493a735a27a62d36e4b7fc9ffbc41033
Su YR <suyr.daniel@gmail.com>
2023-02-12 21:32:04 +0800

    feat: convert input shape to CTHW

--- TubeViT/model.py
+++ TubeViT/model.py
@@ -66,8 +66,7 @@ class SparseTubesTokenizer(nn.Module):
         self.register_parameter('conv_proj_bias', self.conv_proj_bias)
 
     def forward(self, x: Tensor) -> Tensor:
-        n, t, c, h, w = x.shape
-        x = x.permute(0, 2, 1, 3, 4)  # TCHW -> CTHW
+        n, c, t, h, w = x.shape  # CTHW
         tubes = []
         for i in range(len(self.kernel_sizes)):
             if i == 0:
@@ -95,7 +94,7 @@ class TubeViT(nn.Module):
     def __init__(
         self,
         num_classes: int,
-        video_shape: Union[List[int], np.ndarray],  # TCHW
+        video_shape: Union[List[int], np.ndarray],  # CTHW
         num_layers: int,
         num_heads: int,
         hidden_dim: int,
@@ -105,7 +104,7 @@ class TubeViT(nn.Module):
         representation_size=None,
     ):
         super(TubeViT, self).__init__()
-        self.video_shape = np.array(video_shape)
+        self.video_shape = np.array(video_shape)  # CTHW
         self.num_classes = num_classes
         self.hidden_dim = hidden_dim
         self.kernel_sizes = (
@@ -181,7 +180,7 @@ class TubeViT(nn.Module):
         stride = np.array(stride)
         offset = np.array(offset)
 
-        output = np.ceil((self.video_shape[[0, 2, 3]] - offset - kernel_size + 1) / stride).astype(int)
+        output = np.ceil((self.video_shape[[1, 2, 3]] - offset - kernel_size + 1) / stride).astype(int)
         output = np.concatenate([np.array([self.hidden_dim / 6]),
                                  output]).astype(int)  # 6 elements (a sine and cosine value for each x; y; t)
 
