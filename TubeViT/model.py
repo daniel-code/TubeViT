5fa99306221dbe5d3ac306ce498083f23cdad72c
Su YR <suyr.daniel@gmail.com>
2023-03-04 21:15:15 +0800

    feat: weight_decay

--- TubeViT/model.py
+++ TubeViT/model.py
@@ -290,7 +290,7 @@ class TubeViTLightningModule(pl.LightningModule):
         self.log('lr', self.optimizers().optimizer.param_groups[0]['lr'], on_step=False, on_epoch=True)
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        optimizer = optim.Adam(self.model.parameters(), lr=self.lr, weight_decay=self.weight_decay)
         if self.max_epochs is not None:
             lr_scheduler = optim.lr_scheduler.OneCycleLR(optimizer=optimizer,
                                                          max_lr=self.lr,
