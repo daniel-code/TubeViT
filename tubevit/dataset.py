68d85825235b0ed4fa82e3216c329f21f4eff363
Su YR <suyr.daniel@gmail.com>
2026-04-23 21:44:06 +0800

    feat: add Imagenette2Dataset for joint image+video training

--- tubevit/dataset.py
+++ tubevit/dataset.py
@@ -1,7 +1,8 @@
 from typing import Callable, Optional, Tuple
 
 from torch import Tensor
-from torchvision.datasets import UCF101
+from torchvision.datasets import UCF101, ImageFolder
+from torchvision.transforms import v2
 
 
 class MyUCF101(UCF101):
@@ -16,4 +17,23 @@ class MyUCF101(UCF101):
         if self.transform is not None:
             video = self.transform(video)
 
+        return video, label
+
+
+class Imagenette2Dataset(ImageFolder):
+    """Wraps ImageFolder to produce (C, T, H, W) pseudo-videos by repeating a
+    single image T times along the temporal axis."""
+
+    def __init__(self, root: str, frames_per_clip: int = 32, transform: Optional[Callable] = None):
+        super().__init__(root, transform=None)
+        self.clip_transform = transform
+        self.frames_per_clip = frames_per_clip
+
+    def __getitem__(self, idx: int) -> Tuple[Tensor, int]:
+        path, label = self.samples[idx]
+        pil_img = self.loader(path)
+        img = v2.functional.to_image(pil_img)  # (C, H, W) uint8 Tensor
+        if self.clip_transform is not None:
+            img = self.clip_transform(img)  # (C, H, W) float32
+        video = img.unsqueeze(1).repeat(1, self.frames_per_clip, 1, 1)  # (C, T, H, W)
         return video, label
